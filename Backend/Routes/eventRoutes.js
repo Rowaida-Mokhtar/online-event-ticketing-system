5970930f7194cc9773fbe8d037a9bd0dbcdaf7fd
seif <zakysaif61@gmail.com>
2025-04-17 17:29:40 +0200

    remove duplicates in path

--- Backend/Routes/eventRoutes.js
+++ Backend/Routes/eventRoutes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 
 router.post('/', authMiddleware.authenticate, authMiddleware.authorize(['Organizer']), eventController.createEvent);
 router.get('/', eventController.getEvents);
-router.get('/events/:id', eventController.getEventById);
-router.put('/events/:id', authMiddleware.authenticate, authMiddleware.authorize(['Organizer', 'Admin']), eventController.updateEvent);
-router.delete('/events/:id', authMiddleware.authenticate, authMiddleware.authorize(['Organizer', 'Admin']), eventController.deleteEvent);
+router.get('/:id', eventController.getEventById);
+router.put('/:id', authMiddleware.authenticate, authMiddleware.authorize(['Organizer', 'Admin']), eventController.updateEvent);
+router.delete('/:id', authMiddleware.authenticate, authMiddleware.authorize(['Organizer', 'Admin']), eventController.deleteEvent);
 
 module.exports = router;

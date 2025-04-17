b6b0c52729902153cbc0e704ab39f3569f2f07f7
mohamed <mohamedserag20205@gmail.com>
2025-04-17 16:29:05 +0200

    updated event functions

--- Backend/Routes/eventRoutes.js
+++ Backend/Routes/eventRoutes.js
@@ -1,14 +1,14 @@
 //task 2
 const express = require('express');
-const { createEvent, getEvents, getEventById, updateEvent, deleteEvent } = require('../Controllers/eventController');
+const eventController = require('../Controllers/eventController');
 const authMiddleware = require('../Middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('/', authMiddleware.authenticate, authMiddleware.authorize(['Organizer']), createEvent);
-router.get('/', getEvents);
-router.get('/events/:id', getEventById);
-router.put('/events/:id', authMiddleware.authenticate, authMiddleware.authorize(['Organizer', 'Admin']), updateEvent);
-router.delete('/events/:id', authMiddleware.authenticate, authMiddleware.authorize(['Organizer', 'Admin']), deleteEvent);
+router.post('/', authMiddleware.authenticate, authMiddleware.authorize(['Organizer']), eventController.createEvent);
+router.get('/', eventController.getEvents);
+router.get('/events/:id', eventController.getEventById);
+router.put('/events/:id', authMiddleware.authenticate, authMiddleware.authorize(['Organizer', 'Admin']), eventController.updateEvent);
+router.delete('/events/:id', authMiddleware.authenticate, authMiddleware.authorize(['Organizer', 'Admin']), eventController.deleteEvent);
 
 module.exports = router;

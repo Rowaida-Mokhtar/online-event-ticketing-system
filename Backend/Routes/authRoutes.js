7d27e6d3417972e5206cde722a0bcdbaad093782
Rana Abouraia <16abourr@gmail.com>
2025-04-20 20:33:40 +0200

    updated forgetPassword route

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.post('/register',authController.register);
 router.post('/login',authController.login);
-router.post('/forgetPassword',authController.forgetPassword);           //with MFA
+router.put('/forgetPassword',authController.forgetPassword);           //with MFA
 //router.post('/verify-otp',authController.verifyOtp);                  // Step 2: OTP verification
 //router.put('/reset-password',authController.resetPassword);          // Step 3: Reset password
 

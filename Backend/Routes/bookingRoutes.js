a4e5118269bb09bb60e560802ef223b1e1f2a45e
Rana Abouraia <16abourr@gmail.com>
2025-04-21 19:10:00 +0200

    changed booking routes and deleted a file

--- Backend/Routes/bookingRoutes.js
+++ Backend/Routes/bookingRoutes.js
@@ -4,9 +4,9 @@ const authMiddleware = require('../Middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('', authMiddleware.authenticate, bookingController.bookTickets);//make sure its not only users
-router.get('/:id', authMiddleware.authenticate, bookingController.getBookingById);
-router.delete('/:id', authMiddleware.authenticate, bookingController.cancelBooking);
+router.post('', authMiddleware.authenticate,authMiddleware.authorize('user'),bookingController.bookTickets);//make sure its not only users
+router.get('/:id', authMiddleware.authenticate,authMiddleware.authorize('user'),bookingController.getBookingById);
+router.delete('/:id', authMiddleware.authenticate,authMiddleware.authorize('user'),bookingController.cancelBooking);
 
 
 module.exports = router;
